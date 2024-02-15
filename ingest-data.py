b63e92456d54ce528e6d8b0db2b45de5d2ce4ebd
coderjhaw <joshuaehd.silvio.37@gmail.com>
2024-02-15 14:38:35 +0800

    rename notebook and script

--- ingest-data.py
+++ ingest-data.py
@@ -1,8 +1,24 @@
 from sqlalchemy import create_engine
 import pandas as pd
+import json
 
-# create connection to postgres
-engine = create_engine(f'postgresql://postgres:netlab123@172.19.30.77:5432/ticketing')
+# read the json file to python dict
+def get_postgres_credentials(path='config.json'):
+    with open(path, 'r') as file:
+        config = json.load(file)
+    return config['postgres']
+
+# connect to postgres using create_engine
+def connect_to_postgres():
+    credentials = get_postgres_credentials()
+    connection_string = (
+        f"postgresql://{credentials['user']}:{credentials['password']}@{credentials['host']}:{credentials['port']}/{credentials['database']}"
+    )
+    engine = create_engine(connection_string)
+    connection = engine.connect()
+    return connection
+
+engine = connect_to_postgres()
 
 # read the csv file
 df = pd.read_csv('Datasets/tickets.csv')
