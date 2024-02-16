452e7468fd969c670e4a26c9474db27287f45be9
coderjhaw <joshuaehd.silvio.37@gmail.com>
2024-02-16 10:57:04 +0800

    modify script

---
+++ DROP-ENABLE CONSTRAINTS.sql
@@ -0,0 +1,15 @@
+-- Disable foreign key constraints
+ALTER TABLE tickets DISABLE TRIGGER ALL;
+
+ALTER TABLE tickets DROP CONSTRAINT fk_category;
+ALTER TABLE tickets DROP CONSTRAINT fk_deparment;
+ALTER TABLE tickets DROP CONSTRAINT fk_urgency;
+
+TRUNCATE TABLE categories, departments, urgency, tickets;
+
+-- Re-enable foreign key constraints
+ALTER TABLE tickets ENABLE TRIGGER ALL;
+
+ALTER TABLE categories ADD CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(category_id);
+ALTER TABLE departments ADD CONSTRAINT fk_deparment FOREIGN KEY (dept_id) REFERENCES departments(dept_id);
+ALTER TABLE urgency ADD CONSTRAINT fk_urgency FOREIGN KEY (urgency_id) REFERENCES urgency(urgency_id);
--- ingest-data.py
+++ ingest-data.py
@@ -1,24 +1,19 @@
 from sqlalchemy import create_engine
 import pandas as pd
 import json
+import psycopg2
+from db_connection import postgres_connect as pc
 
-# read the json file to python dict
-def get_postgres_credentials(path='config.json'):
-    with open(path, 'r') as file:
-        config = json.load(file)
-    return config['postgres']
+db = pc.get_postgres_credentials()
+conn = psycopg2.connect(
+    host=db['host'],
+    database=db['database'],
+    user=db['user'],
+    password=db['password'],
+    port=db['port'])
 
-# connect to postgres using create_engine
-def connect_to_postgres():
-    credentials = get_postgres_credentials()
-    connection_string = (
-        f"postgresql://{credentials['user']}:{credentials['password']}@{credentials['host']}:{credentials['port']}/{credentials['database']}"
-    )
-    engine = create_engine(connection_string)
-    connection = engine.connect()
-    return connection
-
-engine = connect_to_postgres()
+engine = create_engine(f"postgresql://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}")
+print(engine.connect())
 
 # read the csv file
 df = pd.read_csv('Datasets/tickets.csv')
@@ -37,10 +32,54 @@ df['ticket_id'] = df['ticket_id'].astype('int')
 df['created_date'] = pd.to_datetime(df['created_date'])
 df['resolved_date'] = pd.to_datetime(df['resolved_date'])
 
-df.head(n=0).to_sql(name="tickets_temp")
-df.to_sql(name="tickets_temp", con=engine, if_exists="replace", index=False)
+
+# upload the header of the table
+df.head(n=0).to_sql(name="tickets_temp", con=engine, if_exists="replace", index=False)
+
+# insert data into database
+df.to_sql(name="tickets_temp", con=engine, if_exists="append", index=False)
 
 
 
+# This is for inserting data into separate tables
+try:
+    with conn.cursor() as cursor:
+        # Insert into categories table
+        cursor.execute("""
+            -- Insert into categories table
+            INSERT INTO categories (category_name)
+            SELECT DISTINCT "category_name" FROM tickets_temp
+            WHERE "category_name" NOT IN (SELECT category_name FROM categories);
 
+            -- Insert into departments table
+            INSERT INTO departments (dept_name)
+            SELECT DISTINCT "department_name" FROM tickets_temp
+            WHERE "department_name" NOT IN (SELECT dept_name FROM departments);
 
+            -- Insert into urgency table
+            INSERT INTO urgency (urgency_name)
+            SELECT DISTINCT "Urgency" FROM tickets_temp
+            WHERE "Urgency" NOT IN (SELECT urgency_name FROM urgency);
+
+            -- Insert into tickets table with foreign key references
+            INSERT INTO tickets (ticket_id, created_date, status, subject, resolve_date, remarks, category_id, dept_id, urgency_id)
+            SELECT
+                "ticket_id",
+                "created_date",
+                "Status" as "status",
+                "Subject" as "subject",
+                "resolved_date",
+                "remarks",
+                c.category_id,
+                d.dept_id,
+                u.urgency_id
+            FROM tickets_temp t
+            LEFT JOIN categories c ON t."category_name" = c.category_name
+            LEFT JOIN departments d ON t."department_name" = d.dept_name
+            LEFT JOIN urgency u ON t."Urgency" = u.urgency_name
+            ON CONFLICT (ticket_id) DO NOTHING;""")
+
+                # Commit the changes
+    conn.commit()
+except psycopg2.errors.UniqueViolation as e:
+    print(f"Skipping duplicate ticket_id: {e}")
